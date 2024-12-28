9ca1338d0cf77cb1552db37f1c2997bff01bbb1e
Leo <leo@>
2024-12-28 00:24:24 -0500

    test: Add test to temperature conversion.

--- src/test/java/com/example/unitconverter/UnitConverterApplicationTests.java
+++ src/test/java/com/example/unitconverter/UnitConverterApplicationTests.java
@@ -1,7 +1,9 @@
 package com.example.unitconverter;
 
-import com.example.unitconverter.model.ManageUnits;
+import com.example.unitconverter.model.Length;
+import com.example.unitconverter.model.Temperature;
 import com.example.unitconverter.model.Unit;
+import com.example.unitconverter.model.Weight;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,23 +14,38 @@ class UnitConverterApplicationTests {
 
     @Test
     void testLengthUnits() {
-        Unit length = ManageUnits.createLengthUnits();
-        Assertions.assertEquals(0.1, ManageUnits.convert(length, LengthUnits.CENTIMETER.toString(), LengthUnits.METER.toString(), 10.0));
-        Assertions.assertEquals(304.8, ManageUnits.convert(length, LengthUnits.FOOT.toString(), LengthUnits.METER.toString(), 1000.0));
-        Assertions.assertEquals(36, ManageUnits.convert(length, LengthUnits.YARD.toString(), LengthUnits.INCH.toString(), 1.0));
-        Assertions.assertEquals(1000.0, ManageUnits.convert(length, LengthUnits.METER.toString(), LengthUnits.MILLIMETER.toString(), 1.0));
-        Assertions.assertEquals(63360.0, ManageUnits.convert(length, LengthUnits.MILE.toString(), LengthUnits.INCH.toString(), 1.0));
+        Unit length = new Length();
+        Assertions.assertEquals(roundToPrecision(0.1, length.getPrecisionDecimal()), length.convertTo(LengthUnits.CENTIMETER.toString(), LengthUnits.METER.toString(), 10.0));
+        Assertions.assertEquals(roundToPrecision(304.8, length.getPrecisionDecimal()), length.convertTo(LengthUnits.FOOT.toString(), LengthUnits.METER.toString(), 1000.0));
+        Assertions.assertEquals(roundToPrecision(36, length.getPrecisionDecimal()), length.convertTo(LengthUnits.YARD.toString(), LengthUnits.INCH.toString(), 1.0));
+        Assertions.assertEquals(roundToPrecision(1000.0, length.getPrecisionDecimal()), length.convertTo(LengthUnits.METER.toString(), LengthUnits.MILLIMETER.toString(), 1.0));
+        Assertions.assertEquals(roundToPrecision(63360.0, length.getPrecisionDecimal()), length.convertTo(LengthUnits.MILE.toString(), LengthUnits.INCH.toString(), 1.0));
     }
 
     @Test
     void testWeightUnits() {
-        Unit weight = ManageUnits.createWeightUnits();
-        Assertions.assertEquals(10, ManageUnits.convert(weight, WeightUnits.GRAM.toString(), WeightUnits.KILOGRAM.toString(), 10000.0));
-        Assertions.assertEquals(6.25, ManageUnits.convert(weight, WeightUnits.OUNCE.toString(), WeightUnits.POUND.toString(), 100.0));
-        Assertions.assertEquals(0.01, ManageUnits.convert(weight, WeightUnits.GRAM.toString(), WeightUnits.TON.toString(), 10000.0));
-        Assertions.assertEquals(2.204623, ManageUnits.convert(weight, WeightUnits.KILOGRAM.toString(), WeightUnits.POUND.toString(), 1.0));
-        Assertions.assertEquals(35.273962, ManageUnits.convert(weight, WeightUnits.KILOGRAM.toString(), WeightUnits.OUNCE.toString(), 1.0));
+        Unit weight = new Weight();
+        Assertions.assertEquals(roundToPrecision(10.0, weight.getPrecisionDecimal()), weight.convertTo(WeightUnits.GRAM.toString(), WeightUnits.KILOGRAM.toString(), 10000.0));
+        Assertions.assertEquals(roundToPrecision(6.25, weight.getPrecisionDecimal()), weight.convertTo(WeightUnits.OUNCE.toString(), WeightUnits.POUND.toString(), 100.0));
+        Assertions.assertEquals(roundToPrecision(0.01, weight.getPrecisionDecimal()), weight.convertTo(WeightUnits.GRAM.toString(), WeightUnits.TON.toString(), 10000.0));
+        Assertions.assertEquals(roundToPrecision(2.20462, weight.getPrecisionDecimal()), weight.convertTo(WeightUnits.KILOGRAM.toString(), WeightUnits.POUND.toString(), 1.0));
+        Assertions.assertEquals(roundToPrecision(35.27396, weight.getPrecisionDecimal()), weight.convertTo(WeightUnits.KILOGRAM.toString(), WeightUnits.OUNCE.toString(), 1.0));
 
+    }
+
+    @Test
+    void testTemperatureUnits() {
+        Unit temperature = new Temperature();
+        Assertions.assertEquals(roundToPrecision(33.8, temperature.getPrecisionDecimal()), temperature.convertTo(TemperatureUnits.CELSIUS.toString(), TemperatureUnits.FAHRENHEIT.toString(), 1));
+        Assertions.assertEquals(roundToPrecision(274.15, temperature.getPrecisionDecimal()), temperature.convertTo(TemperatureUnits.CELSIUS.toString(), TemperatureUnits.KELVIN.toString(), 1));
+        Assertions.assertEquals(roundToPrecision(1.0, temperature.getPrecisionDecimal()), temperature.convertTo(TemperatureUnits.FAHRENHEIT.toString(), TemperatureUnits.CELSIUS.toString(), 33.8));
+        Assertions.assertEquals(roundToPrecision(274.15, temperature.getPrecisionDecimal()), temperature.convertTo(TemperatureUnits.FAHRENHEIT.toString(), TemperatureUnits.KELVIN.toString(), 33.8));
+        Assertions.assertEquals(roundToPrecision(33.8, temperature.getPrecisionDecimal()), temperature.convertTo(TemperatureUnits.KELVIN.toString(), TemperatureUnits.FAHRENHEIT.toString(), 274.15));
+        Assertions.assertEquals(roundToPrecision(1.0, temperature.getPrecisionDecimal()), temperature.convertTo(TemperatureUnits.KELVIN.toString(), TemperatureUnits.CELSIUS.toString(), 274.15));
+    }
+
+    public double roundToPrecision(double value, int precision) {
+        return Math.round(value * Math.pow(10, precision)) / Math.pow(10, precision);
     }
 
 
