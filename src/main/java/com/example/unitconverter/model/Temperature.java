f054c79078d00169e9028eb69de985e1ac8adbcb
Leo <leo@>
2024-12-29 19:54:08 -0500

    fix: Validate case when one unit is equal to another unit ignoring capital letters.

--- src/main/java/com/example/unitconverter/model/Temperature.java
+++ src/main/java/com/example/unitconverter/model/Temperature.java
@@ -14,17 +14,20 @@ public class Temperature extends Unit {
     @Override
     public double convertTo(String unit, String otherUnit, double count) {
         double result = 0.0;
-        if (unit.equals(TemperatureUnits.CELSIUS.toString()) && otherUnit.equals(TemperatureUnits.KELVIN.toString())) {
+        if (unit.equalsIgnoreCase(otherUnit)) {
+            return count;
+        }
+        if (unit.equalsIgnoreCase(TemperatureUnits.CELSIUS.toString()) && otherUnit.equalsIgnoreCase(TemperatureUnits.KELVIN.toString())) {
             result = count + 273.15;
-        } else if (unit.equals(TemperatureUnits.CELSIUS.toString()) && otherUnit.equals(TemperatureUnits.FAHRENHEIT.toString())) {
+        } else if (unit.equalsIgnoreCase(TemperatureUnits.CELSIUS.toString()) && otherUnit.equalsIgnoreCase(TemperatureUnits.FAHRENHEIT.toString())) {
             result = count * (9.0 / 5.0) + 32;
-        } else if (unit.equals(TemperatureUnits.FAHRENHEIT.toString()) && otherUnit.equals(TemperatureUnits.CELSIUS.toString())) {
+        } else if (unit.equalsIgnoreCase(TemperatureUnits.FAHRENHEIT.toString()) && otherUnit.equalsIgnoreCase(TemperatureUnits.CELSIUS.toString())) {
             result = 5.0 / 9.0 * (count - 32);
-        } else if (unit.equals(TemperatureUnits.KELVIN.toString()) && otherUnit.equals(TemperatureUnits.CELSIUS.toString())) {
+        } else if (unit.equalsIgnoreCase(TemperatureUnits.KELVIN.toString()) && otherUnit.equalsIgnoreCase(TemperatureUnits.CELSIUS.toString())) {
             result = count - 273.15;
-        } else if (unit.equals(TemperatureUnits.KELVIN.toString()) && otherUnit.equals(TemperatureUnits.FAHRENHEIT.toString())) {
+        } else if (unit.equalsIgnoreCase(TemperatureUnits.KELVIN.toString()) && otherUnit.equalsIgnoreCase(TemperatureUnits.FAHRENHEIT.toString())) {
             result = (count - 273.15) * (9.0 / 5.0) + 32;
-        } else if (unit.equals(TemperatureUnits.FAHRENHEIT.toString()) && otherUnit.equals(TemperatureUnits.KELVIN.toString())) {
+        } else if (unit.equalsIgnoreCase(TemperatureUnits.FAHRENHEIT.toString()) && otherUnit.equalsIgnoreCase(TemperatureUnits.KELVIN.toString())) {
             result = ((5.0 / 9.0) * (count - 32)) + 273.15;
         }
         return Math.round(result * pow(10, precisionDecimal)) / pow(10.0, precisionDecimal);
--- src/main/java/com/example/unitconverter/model/Unit.java
+++ src/main/java/com/example/unitconverter/model/Unit.java
@@ -31,21 +31,25 @@ public class Unit {
     protected void setPrecisionDecimal(int precisionDecimal) {
         this.precisionDecimal = precisionDecimal;
     }
+
     public int getPrecisionDecimal() {
         return precisionDecimal;
     }
 
     public double getUnit(String unit) {
-        return units.get(unit);
+        return units.get(unit.toLowerCase());
     }
 
     public double convertTo(String unit, String otherUnit, double count) {
-        double result;
-        if (baseUnit.equals(unit)) {
+        double result = 0.0;
+        if (unit.equalsIgnoreCase(otherUnit)) {
+            return count;
+        }
+        if (baseUnit.equalsIgnoreCase(unit)) {
             result = count * getUnit(otherUnit);
-        } else if (baseUnit.equals(otherUnit)) {
+        } else if (baseUnit.equalsIgnoreCase(otherUnit)) {
             result = count / getUnit(unit);
-        } else {
+        } else if (units.containsKey(unit.toLowerCase()) && units.containsKey(otherUnit.toLowerCase())) {
             result = count / getUnit(unit) * getUnit(otherUnit);
         }
         return Math.round(result * pow(10.0, precisionDecimal)) / pow(10.0, precisionDecimal);
